0311e8c0fa5f5d5dd5f958f7b52d3113deabe7cf
aoizoijoizj <8519002+aoizoijoizj@users.noreply.github.com>
2020-06-19 10:50:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function removePromotions() {
+  //English
   $('article:has(svg~div:contains("Promoted"))').css('display', 'none');
   $('div[aria-label*="Timeline: Trending now"]>div>div:has(svg~span>span:contains("Promoted"))').css('display', 'none');
   $('aside[aria-label*="Who to follow"]>div>div:has(svg~div>span:contains("Promoted"))').css('display', 'none');
@@ -9,30 +10,9 @@ function removePromotions() {
 window.addEventListener('scroll', removePromotions);
 window.addEventListener('click', function () {
   removePromotions();
-  setTimeout(removePromotions, 250);
-  setTimeout(removePromotions, 500);
-  setTimeout(removePromotions, 750);
-  setTimeout(removePromotions, 1000);
+  for(int i=100; i<5000; i += 100;)setTimeout(removePromotions, i);
 });
 
 // Make sure the promotions are removed on the first page load.
-// Will remove every 500ms for up to the first 10 seconds.
-setTimeout(removePromotions, 500);
-setTimeout(removePromotions, 1000);
-setTimeout(removePromotions, 1500);
-setTimeout(removePromotions, 2000);
-setTimeout(removePromotions, 2500);
-setTimeout(removePromotions, 3000);
-setTimeout(removePromotions, 3500);
-setTimeout(removePromotions, 4000);
-setTimeout(removePromotions, 4500);
-setTimeout(removePromotions, 5500);
-setTimeout(removePromotions, 6000);
-setTimeout(removePromotions, 6500);
-setTimeout(removePromotions, 7000);
-setTimeout(removePromotions, 7500);
-setTimeout(removePromotions, 8000);
-setTimeout(removePromotions, 8500);
-setTimeout(removePromotions, 9000);
-setTimeout(removePromotions, 9500);
-setTimeout(removePromotions, 10000);
+// Will remove every 100ms for up to the first 10 seconds.
+for(int i=100; i<10000; i += 100;)setTimeout(removePromotions, i);
